ace4432f329e5b87d59dda8026b6733ea0c6d18a
wdillard <dillardwa@gmail.com>
2020-11-11 15:17:52 -0600

    clean up un-needed files in Client

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SAApp from './SAApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SAApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
